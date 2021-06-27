c8662ea234f7e86827f56dd304419c3c112cfefd
HirenChafekar <45940947+HirenChafekar@users.noreply.github.com>
2021-06-27 16:43:42 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Shoes in Black Colour",
+      price: 1400,
+      image:
+        "https://rukminim1.flixcart.com/image/714/857/jox7xjk0-1/shoe/h/h/v/ab-12-6-6-blue-chief-blue-original-imaf6gn6tjknjqdz.jpeg?q=50"
+    },
+    {
+      id: "2",
+      name: "Blue Chief Shoes",
+      price: 2400,
+      image:
+        "https://rukminim1.flixcart.com/image/714/857/jox7xjk0/shoe/h/z/b/ab-14-6-6-blue-chief-blue-original-imafb9zshac8nzg8.jpeg?q=50"
+    },
+    {
+      id: "3",
+      name: "Blue Nike",
+      price: 1000,
+      image:
+        "https://c4.wallpaperflare.com/wallpaper/186/622/391/cool-nike-shoes-golden-ball-blue-background-wallpaper-preview.jpg"
+    },
+    {
+      id: "4",
+      name: "Adidas Collection",
+      price: 1000,
+      image:
+        "https://lh3.googleusercontent.com/proxy/oZXNkKPUl3ndj6AWkfoowFfIxZ_bDjPbZDL8VbzPJr5CGv3L-DJoEPX1mSqzYaTNCRx3L1cdWCFZHflskKpgqLgdJwexXWT-QtfkilEBiUw66oAvEAS7iwf4hbTv2g0"
+    },
+    {
+      id: "5",
+      name: "Air Jordan",
+      price: 1000,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeT0R2ESTBhRVgpAbLffTooWqj9d3vucyVhw&usqp=CAU"
+    },
+    {
+      id: "6",
+      name: "The 3-D Shoes",
+      price: 1000,
+      image:
+        "https://3dprint.com/wp-content/uploads/2020/10/futurecraft-strung-3D-printed-shoe-adidas-1024x768.jpg"
+    }
+  ]
+};
+export default data;
